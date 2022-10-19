693960d403218d46851110ee211d28dc9094b8bd
AndreyLytvynov <mrjastonehome@gmail.com>
2022-10-19 22:57:32 +0300

    Выполнено задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function CreateMarkupGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+galleryEl.innerHTML = CreateMarkupGallery(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  overlay: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
